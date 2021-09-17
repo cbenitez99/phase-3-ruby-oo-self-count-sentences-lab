29487c85365cf5db8332e714741862d77cd5666d
Cristian Benitez <cbenitezbusiness9@gmail.com>
2021-09-17 15:34:53 -0500

    commit tree c3543b6e5acd9ce9c4000d3c5607842be1992ec7

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,19 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.include?("!")
   end
 
   def count_sentences
-
+    self.split(/\.|\?|\!/).filter {|words| !words.empty?}.size
+    # binding.pry
   end
 end
